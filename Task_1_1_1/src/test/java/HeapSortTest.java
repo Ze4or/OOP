be728c2f511a294be7e1e76c656cef2a15e6a21d
Kirill Batyaev <k.batyaev@g.nsu.ru>
2026-09-06 17:12:48 +0700

    main files added

---
+++ Task_1_1_1/src/test/java/HeapSortTest.java
@@ -0,0 +1,52 @@
+import org.example.HeapSort;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class HeapSortTest {
+
+    @Test
+    void testSort() {
+        int[] arr = {5, 4, 3, 2, 1};
+
+        HeapSort.heapsort(arr);
+
+        assertArrayEquals(
+                new int[]{1, 2, 3, 4, 5},
+                arr
+        );
+    }
+
+    @Test
+    void testEmpty() {
+        int[] arr = {};
+
+        HeapSort.heapsort(arr);
+
+        assertArrayEquals(new int[]{}, arr);
+    }
+
+    @Test
+    void testDuplicates() {
+        int[] arr = {3, 1, 3, 2, 1};
+
+        HeapSort.heapsort(arr);
+
+        assertArrayEquals(
+                new int[]{1, 1, 2, 3, 3},
+                arr
+        );
+    }
+
+    @Test
+    void testNegativeNumbers() {
+        int[] arr = {-2, 5, -1, 0};
+
+        HeapSort.heapsort(arr);
+
+        assertArrayEquals(
+                new int[]{-2, -1, 0, 5},
+                arr
+        );
+    }
+}
