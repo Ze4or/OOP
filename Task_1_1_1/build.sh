53a2e0101454e27c3f8a7f47834787aeee3a947c
Kirill Batyaev <k.batyaev@g.nsu.ru>
2026-09-09 20:37:13 +0700

    added script

--- Task_1_1_1/build.sh
+++ Task_1_1_1/build.sh
@@ -4,6 +4,4 @@ javadoc -d build/docs/javadoc \
 -sourcepath src/main/java \
 -subpackages ru.nsu.batyaev.task_1_1_1
 
-jar cfe out.jar ru.nsu.batyaev.task_1_1_1.HeapSort -C build ru
-
-java -jar out.jar
+jar cf out.jar -C build ru
--- Task_1_1_1/src/main/java/ru/nsu/batyaev/task_1_1_1/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/batyaev/task_1_1_1/HeapSort.java
@@ -1,7 +1,5 @@
 package ru.nsu.batyaev.task_1_1_1;
 
-import java.util.Arrays;
-
 /**
  * Реализация пирамидальной сортировки.
  */
@@ -57,19 +55,5 @@ public class HeapSort {
 
             heapify(arr, n, largest);
         }
-    }
-
-
-    /**
-     * Точка входа в программу.
-     *
-     * @param args аргументы командной строки
-     */
-    public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-
-        heapsort(arr);
-
-        System.out.println(Arrays.toString(arr));
     }
 }
