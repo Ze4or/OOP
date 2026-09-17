4922bb97ec5dc8366716f9796aea12bfc3598db3
Kirill Batyaev <k.batyaev@g.nsu.ru>
2026-09-17 18:23:43 +0700

    test

--- Task_1_1_2/src/test/java/ru/nsu/batyaev/HandTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/batyaev/HandTest.java
@@ -7,183 +7,137 @@ import static org.junit.jupiter.api.Assertions.*;
 class HandTest {
 
     @Test
-    void newHandShouldHaveZeroScore() {
+    void testAddCard() {
         Hand hand = new Hand();
 
-        assertEquals(0, hand.getScore());
+        Card card = new Card(Card.Suit.HEARTS, Card.Rank.FIVE);
+        hand.addCard(card);
+
+        assertEquals(1, hand.getCards().size());
+        assertEquals(card, hand.getCards().get(0));
     }
 
     @Test
-    void shouldCalculateScoreOfSeveralCards() {
+    void testGetScore() {
         Hand hand = new Hand();
 
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.SEVEN
-        ));
-
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.EIGHT
-        ));
-
-        assertEquals(15, hand.getScore());
-    }
-
-    @Test
-    void aceShouldCountAs11WhenPossible() {
-        Hand hand = new Hand();
-
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.ACE
-        ));
-
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.SIX
-        ));
-
-        assertEquals(17, hand.getScore());
-    }
-
-    @Test
-    void aceShouldCountAs1When11WouldCauseBust() {
-        Hand hand = new Hand();
-
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.ACE
-        ));
-
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.KING
-        ));
-
-        hand.addCard(new Card(
-                Card.Suit.DIAMONDS,
-                Card.Rank.FIVE
-        ));
-
-        assertEquals(16, hand.getScore());
-    }
-
-    @Test
-    void twoAcesShouldHaveScore12() {
-        Hand hand = new Hand();
-
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.ACE
-        ));
-
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.ACE
-        ));
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.FIVE));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.SEVEN));
 
         assertEquals(12, hand.getScore());
     }
 
     @Test
-    void shouldDetectBlackjack() {
+    void testAceAsEleven() {
         Hand hand = new Hand();
 
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.ACE
-        ));
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.ACE));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.FIVE));
 
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.KING
-        ));
+        assertEquals(16, hand.getScore());
+    }
+
+    @Test
+    void testAceAsOne() {
+        Hand hand = new Hand();
+
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.ACE));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.TEN));
+        hand.addCard(new Card(Card.Suit.CLUBS, Card.Rank.FIVE));
+
+        assertEquals(16, hand.getScore());
+    }
+
+    @Test
+    void testSeveralAces() {
+        Hand hand = new Hand();
+
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.ACE));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.ACE));
+        hand.addCard(new Card(Card.Suit.CLUBS, Card.Rank.NINE));
+
+        assertEquals(21, hand.getScore());
+    }
+
+    @Test
+    void testBlackjack() {
+        Hand hand = new Hand();
+
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.ACE));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.KING));
 
         assertTrue(hand.isBlackjack());
     }
 
     @Test
-    void threeCardsWith21ShouldNotBeBlackjack() {
+    void testNotBlackjack() {
         Hand hand = new Hand();
 
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.SEVEN
-        ));
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.TEN));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.NINE));
 
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.SEVEN
-        ));
-
-        hand.addCard(new Card(
-                Card.Suit.DIAMONDS,
-                Card.Rank.SEVEN
-        ));
-
-        assertEquals(21, hand.getScore());
         assertFalse(hand.isBlackjack());
     }
 
     @Test
-    void shouldDetectBust() {
+    void testBlackjackOnlyTwoCards() {
         Hand hand = new Hand();
 
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.KING
-        ));
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.ACE));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.FIVE));
+        hand.addCard(new Card(Card.Suit.CLUBS, Card.Rank.FIVE));
 
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.QUEEN
-        ));
+        assertFalse(hand.isBlackjack());
+    }
 
-        hand.addCard(new Card(
-                Card.Suit.DIAMONDS,
-                Card.Rank.FIVE
-        ));
+    @Test
+    void testBust() {
+        Hand hand = new Hand();
 
-        assertEquals(25, hand.getScore());
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.TEN));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.NINE));
+        hand.addCard(new Card(Card.Suit.CLUBS, Card.Rank.FIVE));
+
         assertTrue(hand.isBust());
     }
 
     @Test
-    void score21ShouldNotBeBust() {
+    void testNotBust() {
         Hand hand = new Hand();
 
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.KING
-        ));
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.TEN));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.SIX));
+        hand.addCard(new Card(Card.Suit.CLUBS, Card.Rank.FIVE));
 
-        hand.addCard(new Card(
-                Card.Suit.CLUBS,
-                Card.Rank.JACK
-        ));
-
-        hand.addCard(new Card(
-                Card.Suit.DIAMONDS,
-                Card.Rank.ACE
-        ));
-
-        assertEquals(21, hand.getScore());
         assertFalse(hand.isBust());
     }
 
     @Test
-    void clearShouldRemoveAllCards() {
+    void testClear() {
         Hand hand = new Hand();
 
-        hand.addCard(new Card(
-                Card.Suit.HEARTS,
-                Card.Rank.TEN
-        ));
+        hand.addCard(new Card(Card.Suit.HEARTS, Card.Rank.FIVE));
+        hand.addCard(new Card(Card.Suit.SPADES, Card.Rank.SIX));
 
         hand.clear();
 
         assertEquals(0, hand.getCards().size());
         assertEquals(0, hand.getScore());
+    }
+
+    @Test
+    void testToString() {
+        Hand hand = new Hand();
+
+        Card card1 = new Card(Card.Suit.HEARTS, Card.Rank.FIVE);
+        Card card2 = new Card(Card.Suit.SPADES, Card.Rank.KING);
+
+        hand.addCard(card1);
+        hand.addCard(card2);
+
+        assertEquals(
+                "[Пятерка Червы (5), Король Пики (10)]",
+                hand.toString()
+        );
     }
 }
